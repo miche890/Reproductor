56791c36f821cf7b1f4fd324b2869a9908631d7f
miche890 <fernandezm525@outlook.es>
2023-02-23 18:19:05 -0500

    23/02/2023 -> implementando a TypeScript

---
+++ ejercicios/typescript/index.ts
@@ -0,0 +1,13 @@
+// Arreglos
+let people: string[] = [];
+people = ['Isabel', 'Nicole', 'Raul'];
+
+enum Color {
+  Rojo = 'Rojo',
+  Verde = 'Verde',
+  Azul = 'Azul',
+  Amarillo = 'Amarillo',
+}
+
+let colorfavorito: Color = Color.Rojo;
+console.log(`Mi color favorito es: ${colorfavorito}`);
